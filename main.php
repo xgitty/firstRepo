7f324f1ecbc7a67a035501a0713b2d8eebb736fe
xgitty <amleela@gmail.com>
2014-07-21 22:41:59 +0100

    initial project

---
+++ main.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once('FirePHPCore/FirePHP.class.php');
+ob_start();
+$firephp = FirePHP::getInstance(true);
+$firephp->log('Hello', 'World');
+
+require_once('FirePHPCore/fb.php');
+$var='test';
+fb($var);
+FB::send($var);
+
+ob_end_flush();
+//comment 2
+
+?>
+
+<script>
+console.log("hiiii");
+</script>
